3c0f039f45875495b2b6c45a4c90ab2e56bc6e9d
cbruce <chrisabruce@gmail.com>
2012-08-31 16:35:10 -0700

    fixes copyright date

--- AceMonsterToys/AMTConstants.h
+++ AceMonsterToys/AMTConstants.h
@@ -1,7 +1,7 @@
 /*************************************************************************
 
  Created by Chris Bruce on 8/31/12
- Copyright (c) 2021 Chris Bruce & Ace Monster Toys, Inc.
+ Copyright (c) 2012 Chris Bruce & Ace Monster Toys, Inc.
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
